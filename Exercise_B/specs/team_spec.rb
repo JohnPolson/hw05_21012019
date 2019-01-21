33e002a4a4465c38441966867e553ea009dc0a38
JohnPolson <JP@JPs-MacBook-Pro.local>
2019-01-21 23:45:15 +0000

    Exercise B completed

--- Exercise_B/specs/team_spec.rb
+++ Exercise_B/specs/team_spec.rb
@@ -5,7 +5,7 @@ require_relative('../team.rb')
 class TestTeam < MiniTest::Test
 
   def setup
-    @record_1 = Team.new('JPs Team', 'JPs Team Coach', ["JP_player01", "JP_player02", "JP_player03"])
+    @record_1 = Team.new('JPs Team', 'JPs Team Coach', ["JP_player01", "JP_player02", "JP_player03"],0)
   end
 
 
@@ -22,15 +22,19 @@ class TestTeam < MiniTest::Test
   end
 
   def test_set_coach_name
-    @record_1.coach_name = 'Coach Alice'
-    assert_equal('Coach Alice', @record_1.coach_name())
+    assert_equal('Coach Alice', @record_1.set_coach_name('Coach Alice'))
   end
 
   def test_add_player
-    new_player = "JP_player04"
-    @record_1.players.push(new_player)
-    assert_equal(["JP_player01", "JP_player02", "JP_player03", "JP_player04"], @record_1.add_players())
+    assert_equal(["JP_player01", "JP_player02", "JP_player03", "JP_player04"], @record_1.add_players("JP_player04"))
   end
+
+  
+
+  def test_winners_points
+        assert_equal(3, @record_1.winners_points("win"))
+      end
+
 
 
 end
--- Exercise_B/team.rb
+++ Exercise_B/team.rb
@@ -1,11 +1,12 @@
 class Team
 
-  attr_accessor :team_name,:coach_name, :players
+  attr_accessor :team_name,:coach_name, :players, :points
 
-def initialize(team_name, coach_name, players)
+def initialize(team_name, coach_name, players, points)
     @team_name = team_name
     @coach_name = coach_name
     @players = players
+    @points = 0
   end
 
   def get_team_name()
@@ -22,12 +23,23 @@ def initialize(team_name, coach_name, players)
 
   def set_coach_name(new_name)
     @coach_name = new_name
+    return @coach_name
   end
 
-  def add_players()
+  def add_players(player)
+    @players.push(player)
     return @players
   end
 
+  
 
+  def winners_points(result)
+       if result == "win"
+         @points += 3
+       else result == "lose"
+         @points += 0
+       end
+     return @points
+   end
 
 end
